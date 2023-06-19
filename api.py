b7c35f194be0793f2501cd1e7160224eb3e1185e
Hariharan0007 <=>
2023-06-19 13:24:37 +0530

    all sorting included

--- api.py
+++ api.py
@@ -1,15 +1,16 @@
 from flask import Flask, jsonify
 import requests
-import datetime
+from datetime import datetime, timedelta
 
-current_time = datetime.datetime.now()
-formatted_time = current_time.strftime("%H:%M:%S")
+
+# current_time = datetime.datetime.now()
+# formatted_time = current_time.strftime("%H:%M:%S")
 
 
 
 app = Flask(__name__)
 API_URL = 'http://104.211.219.98/train/trains'
-BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODcxNTcyNTksImNvbXBhbnlOYW1lIjoiNzEwNzIwMTA0MDI2IiwiY2xpZW50SUQiOiJkOTFlNjY1Ni05MjZkLTQwNDUtYjM3ZS03NzA5OWE4Y2E0ZmYiLCJvd25lck5hbWUiOiIiLCJvd25lckVtYWlsIjoiIiwicm9sbE5vIjoiNzEwNzIwMTA0MDI2In0.fJ14yqH3VJCu29-xg8-_mn1ykmeohUPbI7tx8CAd2X0'
+BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODcxNjEyODYsImNvbXBhbnlOYW1lIjoiNzEwNzIwMTA0MDI2IiwiY2xpZW50SUQiOiJkOTFlNjY1Ni05MjZkLTQwNDUtYjM3ZS03NzA5OWE4Y2E0ZmYiLCJvd25lck5hbWUiOiIiLCJvd25lckVtYWlsIjoiIiwicm9sbE5vIjoiNzEwNzIwMTA0MDI2In0.Se4-gGkgUwo-wE9ZdVaC9lBriBPAxbrXrVaeYFV3kKo'
 
 @app.route('/', methods=['GET'])
 def get_sorted_trains():
@@ -17,28 +18,47 @@ def get_sorted_trains():
     response = requests.get(API_URL, headers=headers)
     response.raise_for_status()
 
-    data = response.json()
+    trains = response.json()
 
-    selected_train = []
-    
-    for train in data:
-        departure_hours = train["departureTime"]["Hours"]
-        departure_minutes = train["departureTime"]["Minutes"]
 
-        departure_time = current_time.replace(hour=departure_hours, minute=departure_minutes)
+    trains = sortTime(trains)
 
-        time_difference = departure_time - current_time
+    sorted_trains = sortTrains(trains)
 
-        print(time_difference)
+    return jsonify(sorted_trains)
 
-        if time_difference <= datetime.timedelta(hours=12):
-            selected_train.append(train)
-        else:
-            print("Departure time is more than 12 hours from the current time.")
+def sortTime(trains):
+    current_time = datetime.now()
+    filtered_trains = []
 
-    
+    for train in trains:
+        departure_time = timedelta(
+            hours=int(train['departureTime']['Hours']),
+            minutes=int(train['departureTime']['Minutes']),
+            seconds=int(train['departureTime']['Seconds'])
+        ) + timedelta(minutes=train['delayedBy'])
 
-    return jsonify(selected_train)
+        time_difference = departure_time.total_seconds() - (current_time.hour * 3600 + current_time.minute * 60 + current_time.second)
+
+        if 1800 <= time_difference <= 12 * 3600:
+            filtered_trains.append(train)
+
+    return filtered_trains
+
+def sortTrains(trains):
+    def sort_key(train):
+        price = train['price']['sleeper']
+        seats_available = train['seatsAvailable']['sleeper']
+        departure_time = train['departureTime']
+        delayed_by = train['delayedBy']
+        return (price, -seats_available, -(departure_time['Hours'] * 3600 + departure_time['Minutes'] * 60 + departure_time['Seconds']) - delayed_by)
+
+    sorted_trains = sorted(trains, key=sort_key)
+    return sorted_trains
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
